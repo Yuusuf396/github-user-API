98d0e2355f22593f27f897d352f0bf1cb2b33e4a
Yuusuf <adebayotoon@gmail.com>
2023-02-22 22:02:46 +0100

    fixing

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,47 +1,71 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
+import { BeatLoader } from 'react-spinners'
 import './style.css'
 
 function Main() {
     const [user,setUser]= useState({})
+    const [error,setErr]= useState(false)
     const [name,setName]= useState('')
+
+    const [filter,setFilter]= useState(false)
     const [idFromButtonClick ,setidFromButtonClick] =useState()
 
-
+   
     const handleClick =()=>{
-        setidFromButtonClick(name)
+        // renderSpinner()
+        setFilter(true);
+        setidFromButtonClick(name);
+        setFilter(false);
+        // if(error===true){
+        //     setFilter(true)
+        // }
     }
-  
     useEffect(()=>{
+        // setFilter(true)
             axios.get(`https://api.github.com/users/${idFromButtonClick}`)
         .then(res => {
-            console.log(res);
+            // console.log(res);
+            
             setUser(res.data)
+            // setName(true)
         })
-        .catch(err => {
+        .catch((err)=> {
             console.log(err)
+            setErr("Couldn't get user ")
         })
        
     },[idFromButtonClick])
+  
+  
  
   return (
     <>
-        <div>Main</div>
+        <div className='main'>Main</div>
         <input type='text' value={name} onChange={e => setName(e.target.value)} />
+        <button onClick={handleClick}  >Search</button>
+        {
+            filter &&
+        <BeatLoader color="#36d7b7" />
+        }
+        {
+            idFromButtonClick  &&
+                <div>
+                    <img alt='nn' src={user.avatar_url} className='avatar'></img>
+                    <p> <a  href={'https://github.com/Yuusuf396'}>official links</a></p>
+                    <div>{user.id}</div>
+                    <div>{user.login}</div>
+                    <div><a href={user.followers_url}> followers-{user.followers}</a></div>
+                    <div><a href={user.following_url}> following-{user.following}</a></div>
+                    <div><a href={user.repos_url}> repos :{user.public_repos}</a></div>
+                </div>
+        }
+       
+        {
+            error &&
+        <p>🌋🔥🔥💣💣{error}💣💣</p>
+        }
 
-        <img alt='nn' src={user.avatar_url} className='avatar'></img>
-        <p> <a  href={'https://github.com/Yuusuf396'}>official links</a></p>
-        <div>{user.id}</div>
-        <div>{user.login}</div>
-        <div><a href={user.followers_url}> followers-{user.followers}</a></div>
-        <div><a href={user.following_url}> following-{user.following}</a></div>
-        <div><a href={user.repos_url}> repos :{user.public_repos}</a></div>
-
-
-
-
-
-        <button onClick={handleClick}>Search</button>
     </>
   )
 }
